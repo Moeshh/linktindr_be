de2db1fbebb5ecb44a153746a75733dca4fe9d6c
Moeshh <mex_ncn@hotmail.com>
2023-02-27 14:42:28 +0100

    first commit

---
+++ dbcontext/Medewerker.cs
@@ -0,0 +1,10 @@
+﻿namespace dbcontext {
+    public class Medewerker {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Postcode { get; set; }
+        public string HouseNumber { get; set; }
+
+    }
+}
---
+++ dbcontext/OurContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbcontext {
+    public class OurContext : DbContext {
+        public OurContext(DbContextOptions options) : base(options) { }
+        public DbSet<Medewerker> medewerkers { get; set; }
+    }
+}
